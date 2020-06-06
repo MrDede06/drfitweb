641fd5f8ddebda213391cff097efad962994d9d3
Ozgur Dede <Ozgur.Dede@gds.ey.com>
2020-06-06 11:27:17 +0200

    Spor Katehori Ekle

--- backend/urls.py
+++ backend/urls.py
@@ -7,7 +7,6 @@ app_name = "backend"
 urlpatterns = [
     path("", views.yonetim, name="yonetim"),
     path("logout/", views.logout_request, name="logout"),
-    path("spor_main/logout/", views.logout_request, name="logout"),
     path("spor_main/", views.spor_main, name="spor_main"),
     path("spor_main/spor_kategori/", spor.kategori_ekle, name="spor_kategori_ekle")
 ]
