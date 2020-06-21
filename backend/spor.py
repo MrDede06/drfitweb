457acdeb79299a08141ad8ba5ab13d57f0eb8e37
Ozgur Dede <Ozgur.Dede@gds.ey.com>
2020-06-21 16:36:00 +0200

    Azure blob storage added

--- backend/spor.py
+++ backend/spor.py
@@ -6,16 +6,25 @@ from django.views.decorators.csrf import csrf_exempt
 import uuid , json , string , random, urllib, base64, os, sys
 from django.utils.encoding import smart_str
 from django.core.files.storage import FileSystemStorage
+from azure.storage.blob import BlobServiceClient, BlobClient
 
 app_name = "backend"
+
+#TODO
+path = r"C:\Users\KG672AV\Desktop\DrFit\repos\myenv\drfitweb"
+string= "DefaultEndpointsProtocol=https;AccountName=drfitstorage;AccountKey=ueV0MNXhWxVWv9Uh+pJMCuB1gPrac+piwdshUeDcqZ06bf3M8/DOaIAHFwyLc+LXZQVAagkt5CI4lDJi0DBgXQ==;EndpointSuffix=core.windows.net"
+#TODO END
+
 
 @staff_member_required
 @csrf_exempt
 def kategori_ekle(request):
+    data = {}
+    
     if request.method == 'GET':
         return render(request = request,
                   template_name='spor_kategori_ekle.html')
-    data = {}
+    
     if request.method == 'POST':
         
         trkateisim = request.POST.get('trkateisim')
@@ -23,17 +32,22 @@ def kategori_ekle(request):
 
         enkateisim = request.POST.get('enkateisim')
         enkateaciklama = request.POST.get('enkateaciklama')
-
-  
-        myfile2 = request.FILES['myfile']
+ 
+        filez = request.FILES['myfile']
         fs = FileSystemStorage()
-        filename = fs.save(myfile2.name, myfile2)
-        print (filename)
-        print(trkateisim)
-        print(trkateaciklama)
-        print(enkateisim)
-        print(enkateaciklama)
-
+        filename = fs.save(filez.name, filez)
+                
+        newBasename = "sporKategory" + trkateisim + ".jpg"
+        newname = os.path.join(path, newBasename)        
+        oldname = os.path.join(path, filename)
+        os.rename(oldname, newname)
+        
+        blob = BlobClient.from_connection_string(container_name="drfitcontainer", conn_str=string, blob_name=newBasename)
+        with open(newname, "rb") as data:
+            blob.upload_blob(data)
+        
+        os.remove(newname)     
+    
 
 
 @staff_member_required
@@ -41,67 +55,6 @@ def altkategori_ekle(request):
     return render(request = request,
                   template_name='spor_altkategori_ekle.html')
 
-@csrf_exempt
-def addcategoryfy(request):
-    data = {}
-    if request.method == 'POST':
-        
-        trkateisim = request.POST.get('trkateisim')
-        trkateaciklama = request.POST.get('trkateaciklama')
-
-        enkateisim = request.POST.get('enkateisim')
-        enkateaciklama = request.POST.get('enkateaciklama')
-
-        filez = request.POST.get('file')
-        print (request.POST)
-        print (filez)
-
-        if trkateisim and filez:
-            filej = json.loads(filez)
-            ftype =  filej['input']['type']
-            imgstring = filej['output']['image']
-            mylist = imgstring.split(',')
-            getjpgorpng = ftype.split('/')
-            imgdata = base64.b64decode(mylist[1])
-            random_img_name = id_generator(22, "qwertyuopasdfghjklizxcvbnm1234567890")
-            filename = '/opt/venv/drfit/drfit/templates/media_cdn/'+random_img_name+'.'+getjpgorpng[1]+''  # I assume you have a way of picking unique filenames
-            filenamefordatabase = ''+random_img_name+'.'+getjpgorpng[1]+''
-            with open(filename, 'wb') as f:
-                f.write(imgdata)
-            try:
-                getcategory = Category.objects.get(name_tr=trkateisim)
-            except Category.DoesNotExist:
-                savecate = Category(name_en=enkateisim,
-                name_tr=trkateisim,
-                explain_en=enkateaciklama,
-                explain_tr = trkateaciklama,
-                image=filenamefordatabase)
-                savecate.save()
-                data["message"] = "Great"
-                data['response'] = "ok"
-                data["filename"] = filenamefordatabase
-                return HttpResponse(json.dumps(data), content_type = "application/json")
-            data = {"response":"Bu kategori zaten var"}
-            return HttpResponse(json.dumps(data), content_type = "application/json")
-        else:
-            data['response'] = "non"
-            data["message"] = "Boş yer bırakmayın"
-            return HttpResponse(json.dumps(data), content_type = "application/json")
-    else:
-        data["message"] = "POST RQ ONLY"
-        data["response"] = "non"
-        return HttpResponse(json.dumps(data), content_type = "application/json")
 
 
-@csrf_exempt
-def form_test(request):
-    if request.method == 'GET':
-         return render(request = request,
-                  template_name='form_test.html')
-    
-    if request.method == 'POST':
-        var1 = request.POST["trkateisim"]
-        var2 = request.POST["trkateaciklama"]
-        print (var1)
-        print (var2)
-     
+
--- backend/urls.py
+++ backend/urls.py
@@ -8,9 +8,7 @@ urlpatterns = [
     path("", views.yonetim, name="yonetim"),
     path("logout/", views.logout_request, name="logout"),
     path("spor_main/", views.spor_main, name="spor_main"),
-    path("addcategoryfy/", spor.addcategoryfy, name='addcategoryfy'),
     path("spor_main/spor_kategori/", spor.kategori_ekle, name="spor_kategori_ekle"),
     path("spor_main/spor_altkategori/", spor.altkategori_ekle, name="spor_altkategori_ekle"),
-    path("spor_main/spor_altkategori/", spor.altkategori_ekle, name="spor_altkategori_ekle"),
-    path("spor_main/form_test/", spor.form_test, name="spor_altkategori_ekle")    
+    path("spor_main/spor_altkategori/", spor.altkategori_ekle, name="spor_altkategori_ekle")
 ]
